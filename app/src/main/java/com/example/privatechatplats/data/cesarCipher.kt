8627a6b388182a088e0b8ee8fc394623a22e4681
AscencioSIUU <131807749+AscencioSIUU@users.noreply.github.com>
2024-10-17 12:47:58 -0600

    OK

--- app/src/main/java/com/example/privatechatplats/data/cesarCipher.kt
+++ app/src/main/java/com/example/privatechatplats/data/cesarCipher.kt
@@ -1,2 +1,17 @@
 package com.example.privatechatplats.data
 
+fun cesarCipher(input: String, shift: Int): String {
+    val result = StringBuilder()
+
+    for (char in input) {
+        if (char.isLetter()) {
+            val base = if (char.isUpperCase()) 'A' else 'a'
+            val shiftedChar = ((char - base + shift) % 26 + base.code).toChar()
+            result.append(shiftedChar)
+        } else {
+            result.append(char)
+        }
+    }
+
+    return result.toString()
+}
